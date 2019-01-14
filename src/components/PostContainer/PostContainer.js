ca9e4fd4968c56705e9feefdd2986b9aab59e053
BrannanC <brannanconrad@gmail.com>
2019-01-14 15:54:11 -0600

    Looks moderately close

--- src/components/PostContainer/PostContainer.js
+++ src/components/PostContainer/PostContainer.js
@@ -4,14 +4,18 @@ import CommentSection from '../CommentSection/CommentSection';
 const PostContainer = (props) => {
     return (
         <div className="PostContainer">
-            <p className="user"><img src={props.postData.thumbnailUrl} alt="User" />{props.postData.username} {props.postData.timestamp}</p>
+            <p className="user"><img src={props.postData.thumbnailUrl} alt="User" /><span className="username">{props.postData.username}</span><span className="timestamp"> {props.postData.timestamp}</span> </p>
             <div className="postImage"><img src={props.postData.imageUrl} alt="Post" /></div>
             <div className="CommentSection">
                 <div className="likes">
+                    <div className="icons">
+                        <i className="far fa-2x fa-heart"></i>
+                        <i className="far  fa-2x fa-comment"></i>
+                    </div>
                     <p>{props.postData.likes} Likes</p>
                 </div>
                 {props.postData.comments.map((x, i)=> <CommentSection comment={x} key={i} />)}
-                <input type="text" placeholder="Add a comment" />
+                <input type="text" placeholder="Add a comment..." />
             </div>
         </div>
     );
